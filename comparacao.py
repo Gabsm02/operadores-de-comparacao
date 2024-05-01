45c488e0693f0c40f55bdbe1ca933b8bdb8df3c1
Gabriel Mendes <gabrielomendes02@gmail.com>
2024-05-01 15:04:29 -0300

    Aprendendo sobre operadores de comparação

---
+++ comparacao.py
@@ -0,0 +1,20 @@
+#Igualdade
+10 == 10 #True
+
+#Diferença
+10 != 10 #False
+
+#Maior que
+10 > 10 #False
+
+#Menor que
+10 < 10 #False
+
+#Maior ou igual
+10 >= 10 #True
+
+#Menor ou igual
+10 <= 10 #True
+
+
+
